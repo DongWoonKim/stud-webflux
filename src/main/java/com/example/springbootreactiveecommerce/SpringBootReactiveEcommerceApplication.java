a6c66922a092c98bdbc15b3d042ab0c919a6c71a
DongWoonKim <01030985843z@naver.com>
2023-02-04 00:07:38 +0900

    chap03 blockhound...

--- src/main/java/com/example/springbootreactiveecommerce/SpringBootReactiveEcommerceApplication.java
+++ src/main/java/com/example/springbootreactiveecommerce/SpringBootReactiveEcommerceApplication.java
@@ -2,11 +2,22 @@ package com.example.springbootreactiveecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.JavaVersion;
+import org.thymeleaf.TemplateEngine;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class SpringBootReactiveEcommerceApplication {
 
     public static void main(String[] args) {
+
+        BlockHound.builder()
+                .allowBlockingCallsInside(
+                        TemplateEngine.class.getCanonicalName(), "process"
+                ).install();
+
+//        BlockHound.install();
+
         SpringApplication.run(SpringBootReactiveEcommerceApplication.class, args);
     }
 
